3e0c1e03b698cf36d29ecedf1fc9b17a4e217699
BimBamBoumBadaboum <39986100+BimBamBoumBadaboum@users.noreply.github.com>
2018-06-06 10:44:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿#region using
+using System;
+using System.IO;
+using Reborn.Database;
+using Reborn.Dofus.D20;
+using Reborn.Dofus.D2P;
+using Reborn.Managers;
+using Reborn.Network;
+#endregion
+
+namespace Reborn
+{
+    class Program
+    {
+        public static Listener Auth = new Listener();
+        public static Listener World = new Listener();
+
+        public static byte[] RawDataMessage = File.ReadAllBytes(Variables.RawDataMessage);
+
+        static void Main(string[] args)
+        {
+            D2pManager.Setup(Variables.Maps);
+            Handle.Setup();
+
+            Auth.Start(Variables.Auth);
+            Console.WriteLine("- - Auth Started - -");
+            World.Start(Variables.World);
+            Console.WriteLine("- - World Started - -");
+
+            Console.WriteLine("= = = LOAD D20 FILES = = =");
+            ObjectDataManager.Initialize(Variables.d2opath);
+
+            EffectsManager.Init();
+            ItemsManager.Init();
+
+            Console.Read();
+        }
+
+
+        //Add plus tard
+        static void Crash(object sender, UnhandledExceptionEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(e.ExceptionObject.ToString());
+            Console.ResetColor();
+
+            //save en cas de crash
+            //Navicat.Save();
+
+            Environment.Exit(1);
+        }
+    }
+}
---
+++ Variables.cs
@@ -0,0 +1,15 @@
+namespace Reborn
+{
+    public class Variables
+    {
+        public static string Key = "649ae451ca33ec53bbcbcc33becf15f4";
+        public static string Maps = @"";
+        public static string d2opath = @"";
+        public static string RawDataMessage = @"";
+
+        public static short Auth = 443;
+        public static short World = 5555;
+
+        public static string Connection = "Server=127.0.0.1;Database=;Uid=;Pwd=;";
+    }
+}
